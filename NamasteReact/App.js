99ebce7048fa10a6aed6f3d96bf2761600ff4ba5
ratnadeep konar <ratnadeep.konar@gmail.com>
2023-03-25 14:49:21 +0530

    Added h1 tag inside a nested div

--- NamasteReact/App.js
+++ NamasteReact/App.js
@@ -1,3 +1,10 @@
-const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+// const heading = React.createElement("h1", {id: "heading"}, "Hello World from React!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement("div", {id: "parent"},
+                React.createElement("div", {id: "child"},
+                    [React.createElement("h1", {id: "heading"}, "I am first h1 tag inside a nested div!"),
+                    React.createElement("h2", {id: "heading"}, "I am sibling of above h1 tag!")]));
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(parent);
