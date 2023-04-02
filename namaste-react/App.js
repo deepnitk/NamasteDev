025e3c0011558189dbbb771a9b4f9ddddc1b785a
ratnadeep konar <ratnadeep.konar@gmail.com>
2023-04-02 15:05:18 +0530

    Laying the foundation | Life before JSX

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"},
-                React.createElement("div", {id: "child"},
-                    [React.createElement("h1", {id: "heading"}, "I love Javascript!"),
-                    React.createElement("h2", {id: "heading"}, "I Love React Library!")]));
+const heading = React.createElement("h1", {id: "heading"}, "Naamaste React 🚀");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(heading);
