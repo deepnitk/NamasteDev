74586d6cd081b5e1347d737adbd9b92c861ad64f
ratnadeep konar <ratnadeep.konar@gmail.com>
2023-04-08 13:00:28 +0530

    Added Html element inside another element

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+const elm = <span>I am a span!</span>
 const title =  (
-    <h1>It's a Title!</h1>
+    <h1>
+        It's a Title!
+        {elm}
+    </h1>
 );
 
 const HeadingComponent = () => {
