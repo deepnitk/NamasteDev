ad03a4b2a171c093be32fe763d2e6e091352d446
ratnadeep konar <ratnadeep.konar@gmail.com>
2023-04-08 13:25:21 +0530

    Called fuctional Component with () inside another functional component

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 const elm = <span>I am a span!</span>
-const title =  (
-    <h1>
+const Title = () =>  {
+    return (<h1>
         It's a Title!
         {elm}
-    </h1>
-);
+    </h1>)
+};
 
 const HeadingComponent = () => {
     return (
         <div>
-            {title}
+            {Title()}
             <h1>{100+200}</h1>
             <h1>Naamaste react 🚀 Functional Component!</h1>
         </div>
