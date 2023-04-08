6b5be0c69d8c629b9eaa3a5a5c5e2857f8d28e21
ratnadeep konar <ratnadeep.konar@gmail.com>
2023-04-08 12:25:39 +0530

    Rendered Functional component in root component

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const heading = React.createElement("h1", {id: "heading"}, "Naamaste React 🚀");
-console.log(heading);
 const jsxHeading = <h1>Naamaste react 🚀</h1>
-console.log(jsxHeading);
+const HeadingComponent = () => {
+    return <h1>Naamaste react 🚀 Functional Component!</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent/>);
+
