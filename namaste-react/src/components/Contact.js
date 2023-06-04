6853a4bfb8db0df591c90d8390e3c6fe130505d0
ratnadek <ratnadeep.konar@gmail.com>
2023-06-04 18:52:33 +0530

    Routes | Added Contact US page route

---
+++ namaste-react/src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return(
+        <div>
+            <h1>Contact US</h1>
+            <h2>Please contact us on <a href="https://www.linkedin.com/in/ratnadeep-konar/">here</a></h2>
+        </div>
+    );
+};
+
+export default Contact;
