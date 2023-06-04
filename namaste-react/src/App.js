757b5f2749b202cb97bfdac0f62858867e235072
ratnadek <ratnadeep.konar@gmail.com>
2023-06-04 18:38:58 +0530

    Routes | Imported {RouterProvider} | Provided the routing configuration to Provider

--- namaste-react/src/App.js
+++ namaste-react/src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, RouterProvider as Provider } from "react-router-dom";
 
 const AppLayout = () => {
     return (
@@ -17,13 +17,13 @@ const AppLayout = () => {
 const appRouter = createBrowserRouter([
     {
         path: "/",
-        element: <AppLayout/>
+        element: <AppLayout/>,
     },
     {
         path: "/about",
-        element: <About/>
+        element: <About/>,
     }
 ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<Provider router={appRouter}/>);
 
