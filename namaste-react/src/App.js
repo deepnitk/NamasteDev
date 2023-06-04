3f94cf47297acbfbe56af373e78dfd688f6652a7
ratnadek <ratnadeep.konar@gmail.com>
2023-06-04 20:13:44 +0530

    Routes | Used Outlet of react-roter-dom to achieve children route functionality
    Now about, contact us route will have Header at top

--- namaste-react/src/App.js
+++ namaste-react/src/App.js
@@ -4,14 +4,14 @@ import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import { createBrowserRouter, RouterProvider as Provider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider as Provider, Outlet } from "react-router-dom";
 import Error from "./components/Error";
 
 const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
         </div>
     )
 }
@@ -20,16 +20,22 @@ const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout/>,
+        children: [
+            {
+                path: "/",
+                element: <Body />,
+            },
+            {
+                path: "/about",
+                element: <About/>,
+            },
+            {
+                path: "/contact",
+                element: <Contact/>,
+            }
+        ],
         errorElement: <Error/>,
-    },
-    {
-        path: "/about",
-        element: <About/>,
-    },
-    {
-        path: "/contact",
-        element: <Contact/>,
-    }
+    }, 
 ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Provider router={appRouter}/>);
