73ad0b40959755684683e920e3412d4b776ffc5a
ratnadek <ratnadeep.konar@gmail.com>
2023-06-28 22:15:19 +0530

    Episode 08 | Part 2 | State variable

--- namaste-react/src/components/UserClass.js
+++ namaste-react/src/components/UserClass.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 class UserClass extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0,
+            count2: 1,
+        };
+    }
+
 
     render() {
+        const {name, location, twitter} = this.props;
+        const {count, count2} = this.state;
         return <div className="userCard">
-            <h2>Name: {this.props.name}</h2>
-            <h2>Location: {this.props.location}</h2>
-            <h2>Twitter: {this.props.twitter}</h2>
+            <h2>Count: {this.state.count}</h2>
+            <h2>Count2: {count2}</h2>
+            <h2>Name: {name}</h2>
+            <h2>Location: {location}</h2>
+            <h2>Twitter: {twitter}</h2>
         </div>
     };
 }
