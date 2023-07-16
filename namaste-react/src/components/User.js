6e76bd8c28035922a75729c8282cb031cc906bdb
ratnadek <ratnadeep.konar@gmail.com>
2023-07-16 13:15:49 +0530

    Remove logs and count variable handling

--- namaste-react/src/components/User.js
+++ namaste-react/src/components/User.js
@@ -2,12 +2,7 @@ import { useState } from "react";
 
 const User = (props) => {
 
-    const [count, setCount] = useState(0);
-    const [count2, setCount2] = useState(1);
-
     return <div className="userCard">
-            <h2>Count: {count}</h2>
-            <h2>Count 2: {count2}</h2>
             <h2>Name: {props.name}</h2>
             <h2>Location: {props.location}</h2>
             <h2>Twitter: {props.twitter}</h2>
--- namaste-react/src/components/UserClass.js
+++ namaste-react/src/components/UserClass.js
@@ -4,46 +4,18 @@ class UserClass extends React.Component {
     
     constructor(props) {
         super(props);
-        this.state = {
-            count: 0,
-        };
-        console.log(this.props.name + ' child constructor is called')
     }
 
-    componentDidMount() {
-        console.log(this.props.name + ' child component coponentDidmount called');
-    }
 
     render() {
-        console.log(this.props.name + ' render method is called');
         const {name, location, twitter} = this.props;
-        const {count, count2} = this.state;
         return <div className="userCard">
-            <h2>Count: {this.state.count}</h2>
             <h2>Name: {name}</h2>
             <h2>Location: {location}</h2>
             <h2>Twitter: {twitter}</h2>
-            <button onClick={()=> {
-                this.setState({
-                    count: this.state.count + 1
-                });
-            }}>+</button>
         </div>
     };
 }
 
-/*
-    -- Parent Costructor Method
-    -- Parent render method
-        -- Child1 constructor method
-        -- Child1 render method
-        
-        -- Child2 constructor method
-        -- Child2 render method
-
-        -- Child1 conponentDidMount method
-        -- Child2  componentDidMount
-    -- Parent componentDidMount method
-*/
 
 export default UserClass;
