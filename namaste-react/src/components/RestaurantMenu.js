bee2eca8d82e6d4b71b9527f461e3b4bb68f28a1
ratnadek <ratnadeep.konar@gmail.com>
2023-07-18 21:48:11 +0530

    Add Custom Hook useRestaurantMenu

--- namaste-react/src/components/RestaurantMenu.js
+++ namaste-react/src/components/RestaurantMenu.js
@@ -1,21 +1,12 @@
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API_URL } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-    const [resInfo, setResInfo] = useState(null);
     const { resId} = useParams();
+    const resInfo = useRestaurantMenu(resId);
 
-    useEffect(() => {
-        fetchMenu();
-    }, []);
-
-    const fetchMenu = async () => {
-        const data = await fetch(MENU_API_URL + resId);
-        const json = await data.json();
-        setResInfo(json.data);
-    };
 
     if (resInfo === null) {
         return <Shimmer/>;
@@ -24,7 +15,6 @@ const RestaurantMenu = () => {
     const { name, cuisines, costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info;
 
     const {itemCards} = resInfo?.cards[3]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-    console.log(itemCards);
     return (
         <div className="menu">
             <h1>{name}</h1>
