338c1c71564b386d51b3725cd4c1046b4d69dbbe
ratnadek <ratnadeep.konar@gmail.com>
2023-08-20 13:29:49 +0530

    Configured React TestingLibrary, jest,Babel,Parcel

--- namaste-react/src/components/Cart.js
+++ namaste-react/src/components/Cart.js
@@ -15,6 +15,7 @@ const Cart = () => {
             <h1 className="text-2xl font-bold">Cart</h1>
             <div className="w-6/12 m-auto">
                 <button className="p-2 m-2 bg-black text-white rounded-lg" onClick={handleClearCart}>Clear Cart</button>
+                {cartItems.length === 0 && <h1>Your cart is empty! Please add items.</h1>}
                 <ItemList items={cartItems}/>
             </div>
         </div>
